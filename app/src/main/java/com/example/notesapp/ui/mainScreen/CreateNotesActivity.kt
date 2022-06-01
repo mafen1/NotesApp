b65f45c196c692f86e3afba3b8f2f8e04ea74955
ok <66274954+mafen1@users.noreply.github.com>
2022-06-01 03:59:40 +0300

    init

--- app/src/main/java/com/example/notesapp/ui/mainScreen/CreateNotesActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/mainScreen/CreateNotesActivity.kt
@@ -1,11 +1,8 @@
 package com.example.notesapp.ui.mainScreen
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.notesapp.databinding.ActivityMainBinding
-import com.example.notesapp.ui.secondScreen.NotesActivity
-import dagger.hilt.android.AndroidEntryPoint
 
 class CreateNotesActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -18,10 +15,6 @@ class CreateNotesActivity : AppCompatActivity() {
         initObserves()
     }
     private fun initView(){
-        binding.floatingActionButton.setOnClickListener {
-            val i = Intent(this, NotesActivity::class.java)
-            startActivity(i)
-        }
     }
     private fun initObserves(){
 
