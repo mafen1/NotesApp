d3c903295a00cea1ca026bd88cf09c1a207ca574
ok <66274954+mafen1@users.noreply.github.com>
2022-06-22 20:27:29 +0300

    init

--- app/src/main/java/com/example/notesapp/presentation/todo/listTodo/TodoAdapter.kt
+++ app/src/main/java/com/example/notesapp/presentation/todo/listTodo/TodoAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.notesapp.presentation.todo.listTodo
 
-class TodoAdapter {
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.data.todo.models.Todo
+import com.example.notesapp.databinding.ItemNotesBinding
+
+class TodoAdapter : ListAdapter<Todo, TodoAdapter.ViewHolder>(ItemComparator()) {
+    val todoList = listOf<Todo>()
+    inner class ViewHolder(private val binding: ItemNotesBinding): RecyclerView.ViewHolder(binding.root)
+
+    class ItemComparator : DiffUtil.ItemCallback<Todo>() {
+        override fun areItemsTheSame(oldItem: Todo, newItem: Todo): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Todo, newItem: Todo): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+  override fun getItemCount(): Int = todoList.size
 }
--- app/src/main/java/com/example/notesapp/presentation/todo/listTodo/TodoViewModel.kt
+++ app/src/main/java/com/example/notesapp/presentation/todo/listTodo/TodoViewModel.kt
@@ -1,9 +1,36 @@
 package com.example.notesapp.presentation.todo.listTodo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notesapp.data.todo.Repository.TodoRepositoryImpl
+import com.example.notesapp.data.todo.models.Todo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TodoViewModel @Inject constructor():ViewModel() {
+class TodoViewModel @Inject constructor(
+//    private val todoRepositoryImpl: TodoRepositoryImpl
+) : ViewModel() {
+
+//    private var _readAllData: MutableLiveData<List<Todo>> = MutableLiveData()
+//    var readAllData: LiveData<List<Todo>> = _readAllData
+//
+//    init {
+//        fetchTodo()
+//    }
+//
+//    private fun fetchTodo(){
+//        viewModelScope.launch(Dispatchers.IO) {
+//            todoRepositoryImpl.readAllData()
+//        }
+//    }
+//    private fun deleteDatabase() {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            todoRepositoryImpl.deleteDataBase()
+//        }
+//    }
 }
